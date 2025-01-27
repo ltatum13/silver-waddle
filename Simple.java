5d5e8366be05321b1e4cf4018dd9acaf850542ea
Laila Tatum <princesslailajoi@gmail.com>
2025-01-27 15:12:34 -0800

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,8 @@
+// this is a comment in Java
+
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Hello World, it's me!");
+        System.out.println("Programming is fun!");
+    }
+}
